9e9145690253343f3880f01cba772d6da2230e02
Jan Dvorak <jan.dvorak@ff.cuni.cz>
2021-07-15 08:56:38 +0200

    Add the generate-diagrams.sh script

---
+++ tools/generate-diagrams.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Generate the svg forms of the diagrams from the source puml files
+#
+# Parameters:
+# - list of paths where to look for a 'diagrams' subdirectory
+#   (the current directory is always included)
+
+for D in . "$@"
+do
+	java -jar ../CERIF-Core/tools/plantuml.jar -v -tsvg "$D/diagrams/**.puml" || \
+	exit 1
+done
