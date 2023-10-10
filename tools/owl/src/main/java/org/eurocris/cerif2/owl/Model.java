2cdd7623cea71978a610ba790fe912ffb17e2525
Jan Dvorak <jan.dvorak@ff.cuni.cz>
2023-10-10 21:18:47 +0200

    Base class IRIs on the filenames

--- tools/owl/src/main/java/org/eurocris/cerif2/owl/Model.java
+++ tools/owl/src/main/java/org/eurocris/cerif2/owl/Model.java
@@ -3,19 +3,21 @@ package org.eurocris.cerif2.owl;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.text.CaseUtils;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.semanticweb.owlapi.vocab.OWL2Datatype;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.vladsch.flexmark.ast.Link;
+import com.vladsch.flexmark.util.ast.Node;
 
 public class Model {
 
@@ -26,6 +28,8 @@ public class Model {
 	private final List<Section> entities = new ArrayList<>();
 
     public static final IRI IRI_BASE = IRI.create( "https://w3id.org/cerif2/" );
+    
+    public static final String DEFAULT_LANGUAGE_CODE = "en";
 	
     private final IRI baseIRI;
 
@@ -51,11 +55,28 @@ public class Model {
 		final Section mainSection = file.getMainSection();
 		entities.add( mainSection );
 
-		final String owlClassName = CaseUtils.toCamelCase( mainSection.getTitle(), true, ' ' );
+		final String owlClassName = file.getPath().getFileName().toString().replaceFirst( "\\.md$", "" );
 		final IRI classIRI = baseIRI.resolve( owlClassName );
 		log.info( "Declaring class '" + owlClassName + "', IRI " + classIRI );
 		final OWLClass owlClass = dataFactory.getOWLClass( classIRI );
 		ont.add( dataFactory.getOWLDeclarationAxiom( owlClass ) );
+		
+		final String classTitle = mainSection.getTitle().trim();
+		final OWLLiteral classTitleLiteral = dataFactory.getOWLLiteral( classTitle + "@" + DEFAULT_LANGUAGE_CODE, OWL2Datatype.RDF_PLAIN_LITERAL );
+		ont.add( dataFactory.getOWLAnnotationAssertionAxiom( owlClass.getIRI(), dataFactory.getRDFSLabel( classTitleLiteral ) ) );
+		
+		final Section definitionSection = mainSection.getSubsectionByTitle( "Definition" );
+		if ( definitionSection != null ) {
+			final StringBuilder sb = new StringBuilder();
+			for ( final Node x : definitionSection.getContents() ) {
+				sb.append( x.getChars().toString() );
+				sb.append( "\n" );
+			}
+			final OWLLiteral definitionLiteral = dataFactory.getOWLLiteral( sb.toString().trim() + "@" + DEFAULT_LANGUAGE_CODE, OWL2Datatype.RDF_PLAIN_LITERAL );
+			ont.add( dataFactory.getOWLAnnotationAssertionAxiom( owlClass.getIRI(), dataFactory.getRDFSComment( definitionLiteral ) ) );
+		} else {
+			throw new IllegalStateException( "Class " + owlClassName + ": definition not found (no subsection titled \"Definition\")" );
+		}
 		
 		final Section specializationOfSection = mainSection.getSubsectionByTitle( "Specialization of" );
 		if ( specializationOfSection != null ) {
