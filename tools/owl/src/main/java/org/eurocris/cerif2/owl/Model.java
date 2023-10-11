2da031596867ec38041c1a688d3db52ffe84ea04
Jan Dvorak <jan.dvorak@ff.cuni.cz>
2023-10-11 16:39:17 +0200

    Process attributes into data properties (1st approximation)

--- tools/owl/src/main/java/org/eurocris/cerif2/owl/Model.java
+++ tools/owl/src/main/java/org/eurocris/cerif2/owl/Model.java
@@ -3,21 +3,32 @@ package org.eurocris.cerif2.owl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.text.CaseUtils;
+import org.jetbrains.annotations.NotNull;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotationValue;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLDataUnionOf;
+import org.semanticweb.owlapi.model.OWLDatatype;
+import org.semanticweb.owlapi.model.OWLDeclarationAxiom;
 import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.semanticweb.owlapi.vocab.OWL2Datatype;
+import org.semanticweb.owlapi.vocab.XSDVocabulary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.vladsch.flexmark.ast.Link;
 import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.sequence.BasedSequence;
+
+import uk.ac.manchester.cs.owl.owlapi.OWLLiteralImplString;
 
 public class Model {
 
@@ -39,11 +50,24 @@ public class Model {
 
     private final OWLDataFactory dataFactory = man.getOWLDataFactory();
 
+	private final OWLDatatype dateDatatype;
+
 	public Model( final String moduleName ) throws OWLOntologyCreationException {
 		super();
 		this.baseIRI = IRI.create( IRI_BASE.toString(), moduleName + "/" );
 		log.info( "Starting model for module '" + moduleName + "', IRI " + baseIRI );
 		this.ont = man.createOntology( baseIRI );
+		
+		final IRI dateDatatypeIRI = baseIRI.resolve( "Date__Datatype" );
+		this.dateDatatype = dataFactory.getOWLDatatype( dateDatatypeIRI );
+		final OWLDataUnionOf owlDataUnionOf = dataFactory.getOWLDataUnionOf(
+				dataFactory.getOWLDatatype( dataFactory.getOWLDatatype( XSDVocabulary.DATE.getIRI() ) ),
+				dataFactory.getOWLDatatype( dataFactory.getOWLDatatype( XSDVocabulary.G_YEAR_MONTH.getIRI() ) ),
+				dataFactory.getOWLDatatype( dataFactory.getOWLDatatype( XSDVocabulary.G_YEAR.getIRI() ) )
+		);
+		if ( moduleName.equals( "CERIF-Core" ) ) {
+			ont.add( dataFactory.getOWLDatatypeDefinitionAxiom(dateDatatype, owlDataUnionOf ) );
+		}
 	}
 
 	public void readInDatatypeFile( final StructuredFile file ) {
@@ -51,7 +75,7 @@ public class Model {
 		datatypes.add( mainSection );
 	}
 
-	public void readInEntityFile( final StructuredFile file ) {
+	public void readInEntityFile( final StructuredFile file ) throws ParseException {
 		final Section mainSection = file.getMainSection();
 		entities.add( mainSection );
 
@@ -88,6 +112,77 @@ public class Model {
 				final OWLClass owlSuperclass = dataFactory.getOWLClass( superclassIRI );
 				ont.add( dataFactory.getOWLSubClassOfAxiom( owlClass, owlSuperclass ) );
 				x = (Link) x.getNextAny( Link.class );
+			}
+		}
+		
+		final Section attributesSection = mainSection.getSubsectionByTitle( "Attributes" );
+		if ( attributesSection != null ) {
+			final List<Node> children = attributesSection.getContents();
+			int i = 0;
+			for ( final Node node : children ) {
+				++i;
+				@NotNull BasedSequence text = node.getChars().trim();
+				log.info( "Node " + node + ": " + text );
+				if ( i == 1 && ( text.startsWith( "Beside" ) || text.startsWith( "Those " ) || text.startsWith( "None besides those of " ) || text.startsWith( "Just those of " ) ) ) {
+					// ignore
+				} else if ( text.startsWith( "FIXME" ) ) {
+					// ignore
+				} else {
+					if ( text.startsWith( "<a name=\"" ) && text.endsWith( "</a>" ) ) {
+						text = text.subSequence( text.indexOf( ">" ) + 1, text.length() - "</a>".length() );
+					}
+					final int colonPosition = text.indexOf( ':' );
+					if ( colonPosition < 0 ) {
+						throw new ParseException( "No colon specified", node );
+					}
+					final String attributeText = text.subSequence( 0, colonPosition ).trim().toString();
+					final BasedSequence rest = text.subSequence( colonPosition + 1 ).trim();
+					final int endashPosition = rest.indexOf( '–' );
+					final BasedSequence datatypeSpec = ( endashPosition > 0 ) ? rest.subSequence( 0, endashPosition ).trim() : rest.trim();
+					final String datatypeLinkTarget = datatypeSpec.toString().replaceAll( "[^(]*\\(([^)]*)\\)", "$1" );
+					final String attributeName = CaseUtils.toCamelCase( attributeText, false, ' ', '-', '_', '.' );
+					log.info( "Attribute " + attributeName + ", datatype " + datatypeLinkTarget );
+					if (! datatypeLinkTarget.startsWith( "../datatypes/" ) ) {
+						throw new ParseException( "Not referencing ../datatypes/", node );
+					}
+					final String datatypeName = datatypeLinkTarget.replaceFirst( "\\.\\./datatypes/(.*)\\.md", "$1" );
+					final OWLDatatype datatype;
+					switch ( datatypeName ) {
+					case "String":
+						datatype = dataFactory.getStringOWLDatatype();
+						break;
+					case "Multilingual_String":
+						datatype = dataFactory.getOWLDatatype( OWL2Datatype.RDF_PLAIN_LITERAL.getIRI() );
+						break;
+					case "Decimal":
+						datatype = dataFactory.getOWLDatatype( OWL2Datatype.XSD_DECIMAL.getIRI() );
+						break;
+					case "Boolean":
+						datatype = dataFactory.getBooleanOWLDatatype();
+						break;
+					case "URI":
+						datatype = dataFactory.getOWLDatatype( OWL2Datatype.XSD_ANY_URI.getIRI() );
+						break;
+					case "Date":
+						datatype = dateDatatype;
+						break;
+					default:
+						datatype = dataFactory.getIntegerOWLDatatype();
+//						throw new IllegalArgumentException( "Unknown datatype " + datatypeName );
+					}
+					
+					final IRI attributeIRI = classIRI.resolve( "#" + attributeName );
+					final OWLDataProperty owlDataProperty = dataFactory.getOWLDataProperty( attributeIRI );
+					final OWLDeclarationAxiom owlAttributeDecl = dataFactory.getOWLDeclarationAxiom( owlDataProperty );
+					ont.add( owlAttributeDecl );
+					ont.add( dataFactory.getOWLDataPropertyDomainAxiom( owlDataProperty, owlClass ) );
+					ont.add( dataFactory.getOWLDataPropertyRangeAxiom( owlDataProperty, datatype ) );
+					
+					if ( endashPosition > 0 ) {
+						final OWLAnnotationValue value = new OWLLiteralImplString( rest.subSequence( endashPosition + 1 ).toString().trim() );
+						ont.add( dataFactory.getOWLAnnotationAssertionAxiom( dataFactory.getRDFSLabel(), attributeIRI, value ) );
+					}
+				}
 			}
 		}
 	}
