32369f01280b9d841afb33c0b813028e28af9015
Jan Dvorak <jan.dvorak@ff.cuni.cz>
2025-09-22 12:31:20 +0200

    Stabilize the appearance of the owl:inverseOf

--- tools/owl/src/main/java/org/eurocris/cerif2/owl/CERIF2Model.java
+++ tools/owl/src/main/java/org/eurocris/cerif2/owl/CERIF2Model.java
@@ -527,11 +527,15 @@ public class CERIF2Model implements AutoCloseable {
 				if ( inverseRangeClassRelationshipsMap != null ) {
 					final Relationship inverseRelationship = inverseRangeClassRelationshipsMap.get( inversePropertyName );
 					if ( inverseRelationship != null ) {
-						
 						final OWLObjectProperty inverseProperty = inverseRelationship.getOwlObjectProperty();
 						if ( inverseProperty != null ) {
-							ont.add( dataFactory.getOWLInverseObjectPropertiesAxiom( owlObjectProperty, inverseProperty ) );
-							ont.add( dataFactory.getOWLInverseObjectPropertiesAxiom( inverseProperty, owlObjectProperty ) );
+							if ( inverseProperty.compareTo( owlObjectProperty ) > 0 ) {
+								ont.add(dataFactory.getOWLInverseObjectPropertiesAxiom(owlObjectProperty, inverseProperty));
+								ont.add(dataFactory.getOWLInverseObjectPropertiesAxiom(inverseProperty, owlObjectProperty));
+							} else {
+								ont.add(dataFactory.getOWLInverseObjectPropertiesAxiom(inverseProperty, owlObjectProperty));
+								ont.add(dataFactory.getOWLInverseObjectPropertiesAxiom(owlObjectProperty, inverseProperty));
+							}
 						}
 					}
 				} else {
